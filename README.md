7135619e503f32dab5623bcb14e0e03441dc7721
dejanle <dejnik978@gmail.com>
2017-12-27 01:12:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+ vue init webpack vue2api
+
+
+? Project name vue2api
+? Project description news site
+? Author dejnik978
+? Vue build standalone
+? Install vue-router? Yes
+? Use ESLint to lint your code? No
+? Set up unit tests No
+? Setup e2e tests with Nightwatch? No
+? Should we run `npm install` for you after the project has been created? (recommended) npm
+
+   vue-cli · Generated "vue2api".
+
+To get started:
+
+  cd vue2api
+  npm run dev
+
+npm install axios --save
+
+npm install --save-dev bootstrap-sass node-sass sass-loader
+
+http://vuetips.com/bootstrap
+https://alligator.io/vuejs/rest-api-axios/
+
+https://github.com/DejanLe/Vue.js-2-rest-api-axios.git
+
+
+
+…or create a new repository on the command line
+
+echo "# Vue.js-2-rest-api-axios" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/DejanLe/Vue.js-2-rest-api-axios.git
+git push -u origin master
+
+…or push an existing repository from the command line
+
+git remote add origin https://github.com/DejanLe/Vue.js-2-rest-api-axios.git
+git push -u origin master
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="news">
+    <div class="container">
+        <div class="row">
+           <div class="col-md-12">
+
+            <ul v-if="posts && posts.length">
+             
+                  <li v-for=" post of posts">
+                    <div class="panel panel-default">
+                      <div class="panel-heading">
+                        <h3> <strong>{{post.title}}  </strong> </h3>
+                      </div>
+                      <div class="panel-body">
+                        <p>{{post.body}}  </p>
+                      </div> 
+                    </div>
+                 </li> 
+              
+            </ul>
+           <ul v-if="errors && errors.length">
+            <li v-for="error of errors"> 
+              {{error.message}}
+            </li>
+           </ul>
+         </div>
+      </div> 
+       
+    </div> 
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+ 
+ export default {
+  data(){
+    return{
+      posts:[],
+      errors: []
+    }
+  },
+  //Fetch posts when the component is created.
+  created(){
+    axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    .then(response =>{
+       this.posts = response.data
+    })
+   .catch(e => {
+    this.errors.push(e)
+   })
+  }
+ }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.panel{
+  max-width:50rem;
+  width:100%; 
+}
+</style>
